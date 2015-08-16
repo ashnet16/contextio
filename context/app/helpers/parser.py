55b2c49c48b20eeec95ade1b9db72f59b4606fbf
loryn808 <loryn808@gmail.com>
2015-08-16 10:32:32 -0400

    added function for initial tone and personality analysis for contact-messages, start to build the jsonOutput

--- context/app/helpers/parser.py
+++ context/app/helpers/parser.py
@@ -1,5 +1,9 @@
 import json
 import re
+import os
+
+from watson.personality import PersonalityInsightsService
+from watson.tone import ToneAnalyzerService
  
 class Parser:
     """Takes in Message JSON object from API and returns requested attributes"""
@@ -9,6 +13,12 @@ class Parser:
         # NB Have to extend pattern to include support for foreign language
         #threadHeaderRegExp = re.compile(r'On\s(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun),\s[A-Z]?[a-z]{2,3}\s[0-9]{1,2},\s[0-9]{4}\sat\s[0-9]{1,2}:[0-9]{2}\s(?:AM|PM),\s[^<]+<[^@]+@[^>]+>\swrote:')
         self.threadHeaderRegExp = re.compile(r'On\s(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun),\s[A-Z]?[a-z]{2,3}\s[0-9]{1,2},\s[0-9]{4}\sat\s[0-9]{1,2}:[0-9]{2}\s(?:AM|PM),\s[^<]+<[^@]+@[^>]+>\swrote:')                    
+       
+        self.toneAnalyzer = ToneAnalyzerService(os.getenv("VCAP_SERVICES"))
+        self.personalityAnalyzer = PersonalityInsightsService(os.getenv("VCAP_SERVICES"))
+
+        self.output = {} # dictionary so we can convert to JSON
+        self.personality = {} # store specific personality traits and score
         
     def extractMessage(self, msg):
         """Helper function for getMsgBodyList to get only the FIRST message in thread"""
@@ -25,3 +35,45 @@ class Parser:
         else:
             cleanMsg = msg
         return cleanMsg
+
+    def getBig5(self, personality):
+        children = personality['tree']['children']['children']
+        for child in children:
+            self.personality[(child['id'])] = child['percentage']
+        return
+           
+
+    def analyzeMessages(self, msgs, **params):
+        self.output = {}
+        getTone = params['tone']
+        message = {} 
+        msgList = []
+        msgContentList = []
+        allContent = '\n'   
+        for m in msgs:
+            if m.get(include_body=1, body_type='text/plain') == False:
+                continue
+            message['datetime'] = m.date 
+            message['subject'] = m.subject
+            message['to'] = params['to']
+            message['from'] = params['from_'] 
+            for mInfo in m.body:
+                content = self.extractMessage(mInfo['content'])
+                message['content'] = content
+                msgContentList.append(content)
+                message['tone'] = json.dumps(self.toneAnalyzer.getTone(content.encode('utf-8')))
+                # to do get average message
+                # aggregate message content
+                allContent = allContent + content
+            msgList.append(message)
+        if params['personality'] == True and len(allContent) > 4000:
+            personalityJson = self.personalityAnalyzer.getProfile(allContent)
+            getBig5(json.loads(personalityJson))
+        #getPersonInfo()
+        return msgContentList
+
+    #def getPersonInfo(self)
+
+
+
+
