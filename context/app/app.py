c9c9e2e0bdcd491d3d3c666e035e5c1a65708147
ashnet16 <ashley@function1.com>
2015-08-25 21:06:11 -0400

    Updated delete logic and styles.

--- context/app/app.py
+++ context/app/app.py
@@ -613,20 +613,20 @@ def enable():
         logger.info(source)
         return json.dumps(source)
 
-@app.route('/delete', methods=["DELETE", "GET"]) #can't remember if delete works in flask. Doesn't with forms. Will test after work.
+@app.route('/delete', methods=['POST', 'GET'])
 def removeAccount():
     removed = ' Your account has been successfully delete. We hope to see you again.'
     error = 'Oops, something went wrong when trying to delete your account. Please contact knowus.io'
     dbremove = dataStore.delete_account(session["context_id"])
     if dbremove == True:
         account = c.Account(context_io, { 'id': session["context_id"]}
-        if account == 'True' # I think it returns boolean. Will check tonight
+        del_acct = account.delete()
+        if del_acct == 'True'
            logger.info('{0} account has been deleted from contextio'.format(session["context_id"]))
            session.clear()
            return render_template('userLogin.html',error=removed)
         else:
             logger.info('Encountered an issue when trying to delete account {0} from contextio.'.format(session["context_id"]))
-                            
     else:
          session.clear()
          return render_template('userLogin.html',error=error)  # Link to html this evening alon with styling
